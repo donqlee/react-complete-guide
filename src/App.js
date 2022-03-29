fb8f0472b3922b4932993850e6c981d2dc1b379d
donqlee <donqlee88@gmail.com>
2022-03-29 23:45:18 +0900

    map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
-const expenses = [
+  const DUMMY_EXPENSES = [
     {
         id: 'e1',
         title: 'Toilet Pape',
@@ -26,11 +26,12 @@ const expenses = [
         date: new Date(2021, 5, 12)
     }
 ]
-
-const addExpenseHandler = expense => {
-  console.log('In App.js');
-  console.log(expense);
-}
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const addExpenseHandler = expense => {
+    setExpenses((prevExpenses => {
+      return [expense, ...prevExpenses];
+    }))
+  }
   return (
     <div>
       <NewExpense onAdddExpense={addExpenseHandler}/>
