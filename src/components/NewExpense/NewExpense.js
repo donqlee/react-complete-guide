112afde46be714ff51bbe8e9241fe4499798dd40
donqlee <donqlee88@gmail.com>
2022-04-03 12:54:08 +0900

    map key

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -8,7 +8,7 @@ const NewExpense = (props) => {
             ...enteredExpenseData,
             id: Math.random().toString()
         }
-        // console.log(expenseData);
+        // console.log(expenseData.id);
         props.onAdddExpense(expenseData);
     };
     return (
