e990c10f413082be3ec17f721f558bb3de6945a4
donqlee <donqlee88@gmail.com>
2022-04-03 18:59:31 +0900

    filterYear filter and if statement

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,18 +11,24 @@ const Expenses = (props) => {
       console.log(selectedYear);
       setFilterYear(selectedYear);
     }
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filterYear;
+    })
+    let expenseContent = <p>No expense Found.</p>
+    if(filteredExpenses.length > 0){
+        expenseContent = filteredExpenses.map((expense) => (
+        <ExpenseItem
+           key={expense.id}
+           title={expense.title}
+           amount={expense.amount}
+           date={expense.date} />                  
+        )); 
+    }
     return (
        
         <Card className="expenses">
             <ExpenseFilter selected = {filterYear} onChangeFilter={filterChangeHandler}/>
-            {props.items.map((expense) => (
-               <ExpenseItem
-               key={expense.id}
-               title={expense.title}
-               amount={expense.amount}
-               date={expense.date} />
-            ))}
-            
+            {expenseContent}
         </Card>
     )
 }
